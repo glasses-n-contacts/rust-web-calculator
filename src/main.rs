ff96160077190b3884a79fca7593b6b97a46a80f
Bill Yu <han.yu@duke.edu>
2018-06-10 14:20:27 +0800

    post request

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,12 @@ extern crate rocket_contrib;
 
 use rocket_contrib::{Json, Template};
 
+#[derive(Deserialize)]
+struct Parameters {
+    a: i32,
+    b: i32,
+}
+
 #[derive(Serialize)]
 struct SerializedResult {
     result: String,
@@ -31,9 +37,14 @@ fn add_json(a: i32, b: i32) -> Json<SerializedResult> {
     Json( SerializedResult { result: (a+b).to_string() })
 }
 
+#[post("/add_post", data = "<params>")]
+fn add_post(params: Json<Parameters>) -> Json<SerializedResult> {
+    Json( SerializedResult { result: (params.a + params.b).to_string() })
+}
+
 fn main() {
     rocket::ignite()
-        .mount("/", routes![index, add_template, add_json])
+        .mount("/", routes![index, add_template, add_json, add_post])
         .attach(Template::fairing())
         .launch();
 }
